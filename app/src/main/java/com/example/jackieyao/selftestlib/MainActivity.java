9125a796bdaa8685c9eeaa456479f00bfbed71a3
jackieyao <1428301152@qq.com>
2018-02-06 20:36:10 +0800

    优化部分代码

--- app/src/main/java/com/example/jackieyao/selftestlib/MainActivity.java
+++ app/src/main/java/com/example/jackieyao/selftestlib/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.jackieyao.selftestlib;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.selftestlib.AdCountDownView;
 
+public class MainActivity extends AppCompatActivity implements AdCountDownView.AnimEndCallBack {
+    AdCountDownView adCountDownView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.test_layout);
+        adCountDownView = findViewById(R.id.ad);
+        adCountDownView.setEndCallBack(this);
+        adCountDownView.setNeedTextStr("s");
+//        adCountDownView.setNeedTextStr("秒");
+    }
+
+
+    public void stroke(View view){
+        adCountDownView.setFlag(AdCountDownView.STROKE);
+        adCountDownView.anim();
+
+    }
+
+    public void solid(View view){
+        adCountDownView.setFlag(AdCountDownView.SOLID);
+        adCountDownView.anim();
+    }
+    public void strokeCircleColor(View view){
+        adCountDownView.setmStrokeCircleColor(Color.YELLOW);
+        adCountDownView.anim();
+    }
+    public void textSize(View view){
+        adCountDownView.setmTextSize(50);
+        adCountDownView.anim();
+    }
+    public void radius(View view){
+        adCountDownView.setRadius(50);
+        adCountDownView.setStrokeWidth(5);
+        adCountDownView.anim();
+    }
+    public void sec(View view){
+        adCountDownView.setIfNeedTextSec(true);
+        adCountDownView.anim();
+    }
+
+    @Override
+    public void animEnd() {
+        Toast.makeText(getApplicationContext(),"动画结束",Toast.LENGTH_LONG).show();
     }
 }
